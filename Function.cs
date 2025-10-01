5537d56bd771637d2cfced0685cae06493e92a89
Volodymyr <yanyshynv@gmail.com>
2025-10-01 17:54:56 +0300

    initial sample with auth

---
+++ Function.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using CloudNative.CloudEvents;
+using Fireball.Game.Server;
+using Fireball.Game.Server.Models;
+using Google.Cloud.Functions.Framework;
+using Google.Cloud.Functions.Hosting;
+using Google.Events.Protobuf.Cloud.PubSub.V1;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SampleProject
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureServices(WebHostBuilderContext context, IServiceCollection services)
+        {
+            services.AddFireballDependencies();
+            services.AddSingleton<IGame, Game>();
+
+            base.ConfigureServices(context, services);
+        }
+    }
+
+    [FunctionsStartup(typeof(Startup))]
+    public class Function : ICloudEventFunction<MessagePublishedData>
+    {
+        private readonly IFireballLogger _logger;
+        private readonly IFireball _fireball;
+        private readonly IGame _game;
+
+        public Function(ILogger<Function> logger, IFireball fireball, IGame game)
+        {
+            _logger = new FireballLogger(nameof(Function), logger); ;
+            _fireball = fireball;
+            _game = game;
+        }
+
+        public async Task HandleAsync(CloudEvent cloudEvent, MessagePublishedData data, CancellationToken cancellationToken)
+        {
+            try
+            {
+                string messageJson = data.Message?.TextData;
+                ParseResult result = await _fireball.ParseMessage(messageJson);
+                _logger.Log($"Message: {result.MessageName}");
+
+                MessageResult messageResult = await _game.HandleMessage(result);
+                if (messageResult != null && messageResult.IsSuccess())
+                {
+                    _logger.Log($"Result: success - {messageResult?.Message}");
+                }
+                else
+                {
+                    _logger.LogError($"Result: error - {messageResult?.Message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception: {ex}");
+            }
+        }
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,69 @@
+﻿using System.Threading.Tasks;
+using Fireball.Game.Server;
+using Fireball.Game.Server.Models;
+using Microsoft.Extensions.Logging;
+
+namespace SampleProject
+{
+    public interface IGame
+    {
+        Task<MessageResult> HandleMessage(ParseResult result);
+    }
+
+    public class Game : IGame
+    {
+        private readonly IFireballLogger _logger;
+        private readonly IFireball _fireball;
+
+        public Game(ILogger<Game> logger, IFireball fireball)
+        {
+            _logger = new FireballLogger(nameof(Game), logger);
+            _fireball = fireball;
+        }
+
+        public async Task<MessageResult> HandleMessage(ParseResult result)
+        {
+            if (result.IsSuccess)
+            {
+                switch (result.MessageName)
+                {
+                    case FireballConstants.MessagesNames.AUTHENTICATE:
+                        return await Auth(result.ToMessage<AuthMessage>());
+                    
+                    case FireballConstants.MessagesNames.SESSION:
+                        return await OnAuthSuccess(result.ToMessage<SessionMessage>());
+                    
+                    case FireballConstants.MessagesNames.AUTHENTICATE_REJECT:
+                        return await OnAuthReject(result.ToMessage<ErrorMessage>());
+
+                    default:
+                        return MessageResult.ErrorResult($"Undefined message name: {result.MessageName}");
+                }
+            }
+
+            return MessageResult.ErrorResult("Error Parse message...");
+        }
+        
+        private async Task<MessageResult> Auth(AuthMessage message)
+        {
+            _logger.Log($"authenticate: {message.ToJson()}");
+
+            // send the authenticating message
+            return await _fireball.Authenticate(message);
+        }
+        
+        private async Task<MessageResult> OnAuthSuccess(SessionMessage message)
+        {
+            if (message.GameSession == null)
+            {
+                // create a new game session
+            }
+            return await _fireball.SendSessionToClient(message);
+        }
+        
+        private async Task<MessageResult> OnAuthReject(ErrorMessage error)
+        {
+            return await _fireball.SendErrorToClient(error, ErrorCode.Authentication);
+        }
+    }
+}
